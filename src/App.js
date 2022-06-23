007498eda17f68334832d6bf00e1c9bbadf2cf52
Jacob Mayeux <jacobmayeux@gmail.com>
2022-06-23 05:55:14 -0500

    Added structure for routes, set up route to LandingPage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
+//Components
+import LandingPageComponent from "./Components/LandingPage/LandingPageComponent.jsx";
+
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPageComponent />}>
+          {/* <Route index element={<Home />} />
+          <Route path="teams" element={<Teams />}>
+            <Route path=":teamId" element={<Team />} />
+            <Route path="new" element={<NewTeamForm />} />
+            <Route index element={<LeagueStandings />} />
+          </Route> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
