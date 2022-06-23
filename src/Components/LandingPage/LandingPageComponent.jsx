ebce7d614d750348279c08aeaa0584c1deecb9ab
Jacob Mayeux <jacobmayeux@gmail.com>
2022-06-23 05:55:46 -0500

    Created path for React Components, Created LandingPageComponents with temp image.

---
+++ src/Components/LandingPage/LandingPageComponent.jsx
@@ -0,0 +1,12 @@
+//Multimedia
+import NotReallyBrothers from "../../Multimedia/Images/NRB.png";
+
+const LandingPageComponent = () => {
+  return (
+    <div className="flex-column flex-center">
+      <img src={NotReallyBrothers} alt="Not Really Brothers" />
+    </div>
+  );
+};
+
+export default LandingPageComponent;
